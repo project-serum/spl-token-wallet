08696e8095b21fc95caaae047a90ff0e0380992f
Nishad <nishadsingh@gmail.com>
2020-09-13 06:15:27 +0800

    Updates

---
+++ precache-manifest.7e07a73baee709952d22ddab9246670e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8747ea4e5742a5c976106b3d530909b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5f5d7f6051752d6527e",
+    "url": "/static/css/main.52d364b1.chunk.css"
+  },
+  {
+    "revision": "a89d723342ce35446395",
+    "url": "/static/js/0.e7920e1c.chunk.js"
+  },
+  {
+    "revision": "110032a9ba4853d39ae5",
+    "url": "/static/js/3.780e165b.chunk.js"
+  },
+  {
+    "revision": "3fa09a418a625df04ac2c2da4c087f17",
+    "url": "/static/js/3.780e165b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f5d7f6051752d6527e",
+    "url": "/static/js/main.36e390c6.chunk.js"
+  },
+  {
+    "revision": "bfd27a2470b23dfc4aab",
+    "url": "/static/js/runtime-main.5b5e4b3d.js"
+  }
+]);
