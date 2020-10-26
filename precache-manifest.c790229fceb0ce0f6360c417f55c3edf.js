6ec348a99ccc430b57e0e5a9759551da81eb5ab8
Nathaniel Parke <nathaniel.parke@gmail.com>
2020-10-26 19:04:23 +0800

    Updates

---
+++ precache-manifest.c790229fceb0ce0f6360c417f55c3edf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e971d9f097a08cdb393e61ade4533e40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f58849affe40835cb90",
+    "url": "/static/css/main.52d364b1.chunk.css"
+  },
+  {
+    "revision": "4e74998684c312f82097",
+    "url": "/static/js/0.3345ea33.chunk.js"
+  },
+  {
+    "revision": "6b0fb4b819300a01d23a",
+    "url": "/static/js/3.7b9f2173.chunk.js"
+  },
+  {
+    "revision": "7329f02e66c5c76ee0e5801a370e030a",
+    "url": "/static/js/3.7b9f2173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f58849affe40835cb90",
+    "url": "/static/js/main.16a87e99.chunk.js"
+  },
+  {
+    "revision": "6f4d7b1e4ad67c3c9f72",
+    "url": "/static/js/runtime-main.6e2dd367.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c0be250f3470bce1fa52a80b48266d4.js"
+  "/precache-manifest.c790229fceb0ce0f6360c417f55c3edf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
