1ac34222070534f42a321a099b1f4546b1457553
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-02-02 19:52:30 +0300

    Format the code better

--- src/components/helpers/import.ts
+++ src/components/helpers/import.ts
@@ -4,7 +4,6 @@ import * as bs58 from 'bs58';
 /**
  * Returns an account object when given the private key
  *
- * @param {string} privateKey - the private key in array format
  */
 export const decodeAccount = (privateKey: string) => {
     try {
