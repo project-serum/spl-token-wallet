4d31a56a2183ecffb779d339643d981f971a3f62
Alvin John <nolasco.ajie@gmail.com>
2021-02-25 22:36:18 +0800

    added submit password when pressed enter key

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -155,48 +155,51 @@ function ChoosePasswordForm({ goBack, onSubmit }) {
 
   return (
     <Card>
-      <CardContent>
-        <Typography variant="h5" gutterBottom>
-          Choose a Password (Optional)
-        </Typography>
-        <Typography>
-          Optionally pick a password to protect your wallet.
-        </Typography>
-        <TextField
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          label="New Password"
-          type="password"
-          autoComplete="new-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <TextField
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          label="Confirm Password"
-          type="password"
-          autoComplete="new-password"
-          value={passwordConfirm}
-          onChange={(e) => setPasswordConfirm(e.target.value)}
-        />
-        <Typography>
-          If you forget your password you will need to restore your wallet using
-          your seed words.
-        </Typography>
-      </CardContent>
-      <CardActions style={{ justifyContent: 'space-between' }}>
-        <Button onClick={goBack}>Back</Button>
-        <Button
-          color="primary"
-          disabled={password !== passwordConfirm}
-          onClick={() => onSubmit(password)}
-        >
-          Create Wallet
-        </Button>
-      </CardActions>
+      <form onSubmit={(e) => [e.preventDefault(), onSubmit(password)]}>
+        <CardContent>
+          <Typography variant="h5" gutterBottom>
+            Choose a Password (Optional)
+          </Typography>
+          <Typography>
+            Optionally pick a password to protect your wallet.
+          </Typography>
+          <TextField
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            label="New Password"
+            type="password"
+            autoComplete="new-password"
+            autoFocus
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            label="Confirm Password"
+            type="password"
+            autoComplete="new-password"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+          />
+          <Typography>
+            If you forget your password you will need to restore your wallet using
+            your seed words.
+          </Typography>
+        </CardContent>
+        <CardActions style={{ justifyContent: 'space-between' }}>
+          <Button onClick={goBack}>Back</Button>
+          <Button
+            color="primary"
+            disabled={password !== passwordConfirm}
+            type="submit"
+          >
+            Create Wallet
+          </Button>
+        </CardActions>
+      </form>
     </Card>
   );
 }
@@ -215,35 +218,38 @@ function LoginForm() {
 
   return (
     <Card>
-      <CardContent>
-        <Typography variant="h5" gutterBottom>
-          Unlock Wallet
-        </Typography>
-        <TextField
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={stayLoggedIn}
-              onChange={(e) => setStayLoggedIn(e.target.checked)}
-            />
-          }
-          label="Keep wallet unlocked"
-        />
-      </CardContent>
-      <CardActions style={{ justifyContent: 'flex-end' }}>
-        <Button color="primary" onClick={submit}>
-          Unlock
-        </Button>
-      </CardActions>
+      <form onSubmit={(e) => [e.preventDefault(), submit()]}>
+        <CardContent>
+          <Typography variant="h5" gutterBottom>
+            Unlock Wallet
+          </Typography>
+          <TextField
+            variant="outlined"
+            autoFocus
+            fullWidth
+            margin="normal"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={stayLoggedIn}
+                onChange={(e) => setStayLoggedIn(e.target.checked)}
+              />
+            }
+            label="Keep wallet unlocked"
+          />
+        </CardContent>
+        <CardActions style={{ justifyContent: 'flex-end' }}>
+          <Button color="primary" type="submit">
+            Unlock
+          </Button>
+        </CardActions>
+      </form>
     </Card>
   );
 }
