44f6a8403c0028da203ea8897b827afe3a86b846
Nathaniel Parke <nathaniel.parke@gmail.com>
2021-03-12 07:57:42 +0800

    Migrate to native USDT

--- src/utils/tokens/names.js
+++ src/utils/tokens/names.js
@@ -62,9 +62,16 @@ export const TOKENS = {
         'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ripple/info/logo.png',
     },
     {
-      tokenSymbol: 'USDT',
+      tokenSymbol: 'WUSDT',
       mintAddress: 'BQcdHdAQW1hczDbBi9hiegXAR7A98Q9jx3X3iBBBDiq4',
       tokenName: 'Wrapped USDT',
+      icon:
+        'https://raw.githubusercontent.com/trustwallet/assets/f3ffd0b9ae2165336279ce2f8db1981a55ce30f8/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png',
+    },
+    {
+      tokenSymbol: 'USDT',
+      mintAddress: 'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB',
+      tokenName: 'USD Tether',
       icon:
         'https://raw.githubusercontent.com/trustwallet/assets/f3ffd0b9ae2165336279ce2f8db1981a55ce30f8/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png',
     },
