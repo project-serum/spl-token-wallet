543a0a8f1e8e6f5c6be5015ea0a3fc16f91c508b
Paul Bernier <bernier.pja@gmail.com>
2021-04-12 00:38:54 -0700

    fix 'sign' method in Chrome extension context (#177)

--- src/pages/PopupPage.js
+++ src/pages/PopupPage.js
@@ -35,7 +35,23 @@ function getInitialRequests() {
   // TODO CHECK OPENER (?)
 
   const urlParams = new URLSearchParams(window.location.hash.slice(1));
-  return [JSON.parse(urlParams.get('request'))];
+  const request = JSON.parse(urlParams.get('request'));
+  
+  if (request.method === 'sign') {
+    const dataObj = request.params.data;
+    // Deserialize `data` into a Uint8Array
+    if (!dataObj) {
+      throw new Error('Missing "data" params for "sign" request');
+    }
+
+    const data = new Uint8Array(Object.keys(dataObj).length);
+    for (const [index, value] of Object.entries(dataObj)) {
+      data[index] = value;
+    }
+    request.params.data = data;
+  }
+
+  return [request];
 }
 
 export default function PopupPage({ opener }) {
