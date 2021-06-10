cf167e59f4969792badd21c4bb6c6be940f98ede
CCAI <ccaideveloper@gmail.com>
2021-06-10 22:27:09 +0300

    removed mango and orca rpcs

--- src/utils/connection.tsx
+++ src/utils/connection.tsx
@@ -42,12 +42,12 @@ export function ConnectionProvider({ children }) {
         ? // multi connection only for mainnet
           new MultiEndpointsConnection(
             [
-              { url: 'https://mango.rpcpool.com/', RPS: 10 },
+              // { url: 'https://mango.rpcpool.com/', RPS: 10 },
               { url: 'https://solana-api.projectserum.com', RPS: 2 },
               { url: 'https://api.mainnet-beta.solana.com', RPS: 4 },
               // { url: 'https://raydium.rpcpool.com/', RPS: 10 },
-              { url: 'https://orca.rpcpool.com/', RPS: 10 },
-              { url: 'https://api.rpcpool.com', RPS: 10 },
+              // { url: 'https://orca.rpcpool.com/', RPS: 10 },
+              // { url: 'https://api.rpcpool.com', RPS: 10 },
             ],
             'recent',
           )
