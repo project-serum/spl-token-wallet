7599d801d76935878fde1cc5e5577cceb9242a91
Arrowana <8245419+Arrowana@users.noreply.github.com>
2021-07-30 20:52:21 +1000

    Add copy on click on account, and show shortened address

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -111,3 +111,8 @@ export const decodeAccount = (privateKey: string) => {
     }
   }
 }
+
+// shorten the checksummed version of the input address to have 4 characters at start and end
+export function shortenAddress(address: string, chars = 4): string {
+  return `${address.slice(0, chars)}...${address.slice(-chars)}`;
+}
