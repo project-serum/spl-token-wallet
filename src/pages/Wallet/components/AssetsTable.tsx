b78a13a5f5d78c352b5df750ac39380195de15aa
CCAI <ccaideveloper@gmail.com>
2021-08-02 12:45:47 +0300

    added close token account button

--- src/pages/Wallet/components/AssetsTable.tsx
+++ src/pages/Wallet/components/AssetsTable.tsx
@@ -8,9 +8,7 @@ import { Row, RowContainer, Title, VioletButton } from '../../commonStyles';
 import TokenIcon from '../../../components/TokenIcon';
 import { useWallet } from '../../../utils/wallet';
 
-import {
-  useSolanaExplorerUrlSuffix,
-} from '../../../utils/connection';
+import { useSolanaExplorerUrlSuffix } from '../../../utils/connection';
 import {
   formatNumberToUSFormat,
   isUSDToken,
@@ -279,6 +277,7 @@ const AssetsTable = ({
   setSendDialogOpen,
   setDepositDialogOpen,
   setShowAddTokenDialog,
+  setCloseTokenAccountDialogOpen,
 }: {
   isActive?: boolean;
   allTokensData: Map<string, TokenInfo>;
@@ -294,6 +293,7 @@ const AssetsTable = ({
   setSendDialogOpen: (isOpen: boolean) => void;
   setDepositDialogOpen: (isOpen: boolean) => void;
   setShowAddTokenDialog: (isOpen: boolean) => void;
+  setCloseTokenAccountDialogOpen: (isOpen: boolean) => void;
 }) => {
   const theme = useTheme();
   const wallet = useWallet();
@@ -404,6 +404,7 @@ const AssetsTable = ({
                 selectToken={selectToken}
                 setSendDialogOpen={setSendDialogOpen}
                 setDepositDialogOpen={setDepositDialogOpen}
+                setCloseTokenAccountDialogOpen={setCloseTokenAccountDialogOpen}
               />
             ))}
             {sortedPublicKeys.length === 0 && (
@@ -435,6 +436,7 @@ const AssetItem = ({
   selectToken,
   setSendDialogOpen,
   setDepositDialogOpen,
+  setCloseTokenAccountDialogOpen,
   marketsData = new Map(),
   balanceInfo,
 }: {
@@ -451,6 +453,7 @@ const AssetItem = ({
   }) => void;
   setSendDialogOpen: (isOpen: boolean) => void;
   setDepositDialogOpen: (isOpen: boolean) => void;
+  setCloseTokenAccountDialogOpen: (isOpen: boolean) => void;
 }) => {
   const wallet = useWallet();
   const urlSuffix = useSolanaExplorerUrlSuffix();
@@ -713,6 +716,28 @@ const AssetItem = ({
             Send
           </VioletButton>
 
+          {mint && amount === 0 && (
+            <VioletButton
+              theme={theme}
+              height="50%"
+              width="7rem"
+              background={
+                'linear-gradient(140.41deg, #F26D68 0%, #F69894 92.17%)'
+              }
+              hoverBackground={
+                'linear-gradient(140.41deg, #F26D68 0%, #F69894 92.17%)'
+              }
+              margin="0 2rem 0 0"
+              onClick={() => {
+                selectToken({ publicKey, isAssociatedToken });
+                setCloseTokenAccountDialogOpen(true);
+              }}
+            >
+              <img src={SendIcon} alt="send" style={{ marginRight: '.5rem' }} />
+              Delete
+            </VioletButton>
+          )}
+
           <VioletButton
             theme={theme}
             component="a"
@@ -785,7 +810,9 @@ const AssetItem = ({
 export default React.memo(AssetsTable, (prev, next) => {
   return (
     prev.isActive === next.isActive &&
-    JSON.stringify([...prev.allTokensData.values()]) === JSON.stringify([...next.allTokensData.values()]) &&
-    JSON.stringify([...prev.marketsData.values()]) === JSON.stringify([...next.marketsData.values()])
+    JSON.stringify([...prev.allTokensData.values()]) ===
+      JSON.stringify([...next.allTokensData.values()]) &&
+    JSON.stringify([...prev.marketsData.values()]) ===
+      JSON.stringify([...next.marketsData.values()])
   );
 });
