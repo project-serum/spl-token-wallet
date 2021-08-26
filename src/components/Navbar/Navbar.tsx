a1faefa7c9faa49119a049124fb7b3442335a29a
CCAI <ccaideveloper@gmail.com>
2021-08-26 19:04:20 +0300

    show rebalance link in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -248,21 +248,19 @@ const Navbar = () => {
                 Analytics
               </NavLinkButton>
             </StyledLink>
-            {!MASTER_BUILD && (
-              <StyledLink href={`https://dex.aldrin.com/rebalance`}>
-                <NavLinkButton
-                  theme={theme}
-                  page={'rebalance'}
-                  pathname={location.pathname}
-                  style={{ width: '14rem' }}
-                >
-                  <>
-                    Rebalance{' '}
-                    <BetaLabel theme={theme} style={{ marginLeft: '1rem' }} />
-                  </>
-                </NavLinkButton>
-              </StyledLink>
-            )}
+            <StyledLink href={`https://dex.aldrin.com/rebalance`}>
+              <NavLinkButton
+                theme={theme}
+                page={'rebalance'}
+                pathname={location.pathname}
+                style={{ width: '14rem' }}
+              >
+                <>
+                  Rebalance{' '}
+                  <BetaLabel theme={theme} style={{ marginLeft: '1rem' }} />
+                </>
+              </NavLinkButton>
+            </StyledLink>
             <Link to={`/`} style={{ height: '100%' }}>
               <NavLinkButton
                 theme={theme}
